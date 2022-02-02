db3ec320b7b083e0acff82417d0adb212c18f2a6
iamarthurk <iamarthurk@hey.com>
2022-02-02 09:21:19 +0400

    chore: stack and tooling setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Hello world! text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/Hello world!/i);
+  expect(textElement).toBeInTheDocument();
 });
